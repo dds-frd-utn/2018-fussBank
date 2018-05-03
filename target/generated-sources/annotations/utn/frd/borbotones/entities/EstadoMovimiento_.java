3669fa9396caa2220099097642ae6a3d23392a4c
Agustin Gambirassi <gambirassi.a@gmail.com>
2018-05-02 21:55:49 -0300

    Agregado clase CORS para poder trabajar desde el front end

--- target/generated-sources/annotations/utn/frd/borbotones/entities/EstadoMovimiento_.java
+++ target/generated-sources/annotations/utn/frd/borbotones/entities/EstadoMovimiento_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-27T18:05:06")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-05-01T02:19:12")
 @StaticMetamodel(EstadoMovimiento.class)
 public class EstadoMovimiento_ { 
 
